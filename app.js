f39d0df1ff8fac4b7680ff2b47930db6a8b7b198
Jules <j.marshallmiami@yahoo.com>
2022-06-01 22:04:23 -0500

    template can accept header and about section

--- app.js
+++ app.js
@@ -1,16 +1,7 @@
 const inquirer = require('inquirer');
 
-// const fs = require('fs');
-// const generatePage = require('./src/page-template.js');
-
-// const pageHTML = generatePage(name, github)
-
-
-// fs.writeFile('index.html', pageHTML, err => {
-//     if (err) throw new Error(err);
-
-//     console.log('Portfolio complete! Check out index.html to see output!');
-// });
+const fs = require('fs');
+const generatePage = require('./src/page-template.js');
 
 const promptUser = () => {
     return inquirer.prompt([
@@ -141,12 +132,12 @@ Add a New Project
 promptUser()
     .then(promptProject)
     .then(portfolioData => {
-        console.log(portfolioData);
-        // will be uncommented in lesson 4
-        // const pageHTML = generatePage(portfolioData);
-        // fs.writeFile('./index.html', pageHTML, err => {
-        //   if (err) throw new Error(err);
-        //   console.log('Page created! Check out index.html in this directory to see it!');
-        // });
+
+        const pageHTML = generatePage(portfolioData);
+
+        fs.writeFile('./index.html', pageHTML, err => {
+          if (err) throw new Error(err);
+          console.log('Page created! Check out index.html in this directory to see it!');
+        });
     });
 
--- src/page-template.js
+++ src/page-template.js
@@ -1,4 +1,23 @@
-const generatePage = (name, github) => {
+// create about section
+const generateAbout = aboutText => {
+    if (!aboutText) {
+        return '';
+    }
+    return `
+        <section class="my-3" id="about">
+            <h2 class="text-dark bg-primary p-2 display-inline-block">About Me</h2>
+            <p>${aboutText}</p>
+        </section>
+    `;
+};
+
+module.exports = templateData => {
+    console.log(templateData);
+
+    const { projects, about, ...header } = templateData;
+
+
+
     return `
     <!DOCTYPE html>
     <html lang="en">
@@ -10,14 +29,23 @@ const generatePage = (name, github) => {
         <title>Portfolio Demo</title>
     </head>
     
-    <body>
-        <h1>${name}</h1>
-        <h2><a href = "https://github.com/${github}">Github</a></h2>
-
-    </body>
-    
-    </html>
-    `;
+  <body>
+    <header>
+      <div class="container flex-row justify-space-between align-center py-3">
+        <h1 class="page-title text-secondary bg-dark py-2 px-3">${header.name}</h1>
+        <nav class="flex-row">
+          <a class="ml-2 my-1 px-2 py-1 bg-secondary text-dark" href="https://github.com/${header.github
+        }">GitHub</a>
+        </nav>
+      </div>
+    </header>
+    <main class="container my-5">
+        ${generateAbout(about)}
+    </main>
+    <footer class="container text-center py-3">
+      <h3 class="text-dark">&copy; ${new Date().getFullYear()} by ${header.name}</h3>
+    </footer>
+  </body>
+  </html>
+  `;
 };
-
-module.exports = generatePage;
